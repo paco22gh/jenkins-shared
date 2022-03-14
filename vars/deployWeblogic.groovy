7fc8a1a5d02b74736e7a73c974ef4ca58f327a6d
paco22gh <61763224+paco22gh@users.noreply.github.com>
2022-03-14 14:01:29 -0600

    Update deployWeblogic.groovy
    more params

--- vars/deployWeblogic.groovy
+++ vars/deployWeblogic.groovy
@@ -1,7 +1,8 @@
 
 def call(Map config = [:]) {
 
-    sh "curl -v -u ${config.user}:${config.password} -T /root/tomcat/wars/mvnwebapp.war 'http://localhost:8081/manager/text/deploy?path=/myapp&update=true'"
+    String destination = 'http://localhost:8081/manager/text/deploy?path=/myapp&update=true'
+    sh "curl -v -u ${config.user}:${config.password} -T /root/tomcat/wars/mvnwebapp.war ${destination}"
     echo "User is , ${config.user}."
 
 }
