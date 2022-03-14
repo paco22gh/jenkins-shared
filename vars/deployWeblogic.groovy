8a4999ce98fa79cdf112633494e6b05bc53c0a09
paco22gh <61763224+paco22gh@users.noreply.github.com>
2022-03-14 13:59:01 -0600

    Update deployWeblogic.groovy
    validated script using params user and password

--- vars/deployWeblogic.groovy
+++ vars/deployWeblogic.groovy
@@ -1,7 +1,7 @@
 
 def call(Map config = [:]) {
 
-    sh "curl -v -u ${config.user}:deploy -T /root/tomcat/wars/mvnwebapp.war 'http://localhost:8081/manager/text/deploy?path=/myapp&update=true'"
+    sh "curl -v -u ${config.user}:${config.password} -T /root/tomcat/wars/mvnwebapp.war 'http://localhost:8081/manager/text/deploy?path=/myapp&update=true'"
     echo "User is , ${config.user}."
 
 }
